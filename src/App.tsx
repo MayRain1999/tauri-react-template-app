18f6a6c7d68ec39382292b9bfaa0d354f19c6032
xiaoping027 <zhangxiaoping0207@gmail.com>
2023-06-19 23:25:04 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import * as reactLogo from "@/assets/react.svg";
+import { invoke } from "@tauri-apps/api/tauri";
+import "./App.css";
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  return (
+    <div className="container bg-black p-6">
+      <div className="p-6">Welcome to Tauri!</div>
+      <img src={reactLogo} alt="" />
+      <form
+        className="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          greet();
+        }}
+      >
+        <input
+          id="greet-input"
+          onChange={(e) => setName(e.currentTarget.value)}
+          placeholder="Enter a name..."
+        />
+        <button type="submit">Greet</button>
+      </form>
+
+      <p>{greetMsg}</p>
+    </div>
+  );
+}
+
+export default App;
