f0ef3d09e770bef013c3b16cf381568379b43a03
mayrain <zhangxiaoping0207@gmail.com>
2023-06-19 23:50:43 +0800

    fix eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,29 @@
-import React, { useState } from "react";
-import * as reactLogo from "@/assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
+import React, { useState } from 'react'
+import { invoke } from '@tauri-apps/api/tauri'
+import './App.css'
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [greetMsg, setGreetMsg] = useState('')
+  const [name, setName] = useState('')
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+    setGreetMsg(await invoke('greet', { name }))
   }
 
   return (
     <div className="container bg-black p-6">
       <div className="p-6">Welcome to Tauri!</div>
-      <img src={reactLogo} alt="" />
       <form
         className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
+        onSubmit={e => {
+          e.preventDefault()
+          greet()
         }}
       >
         <input
           id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
+          onChange={e => setName(e.currentTarget.value)}
           placeholder="Enter a name..."
         />
         <button type="submit">Greet</button>
@@ -33,7 +31,7 @@ function App() {
 
       <p>{greetMsg}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
